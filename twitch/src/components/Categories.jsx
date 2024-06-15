29240f3e77b362063beb0a011902407fb6d57b1b
Luis Casas <luisscasas45@gmail.com>
2024-06-15 17:11:46 -0600

    FEAT/ responsive Categories

--- twitch/src/components/Categories.jsx
+++ twitch/src/components/Categories.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 function Categories() {
   return (
-    <div className=''>
+    <div className='p-4'>
       
-      <div className='ml-8 mt-10 mb-1'>
-        <p className="text-6xl font-semibold">EXPLORE</p>
+      <div className='ml-4 mt-10 mb-1 sm:ml-8'>
+        <p className="text-3xl sm:text-6xl font-semibold">EXPLORE</p>
       </div>
-      <div className="flex gap-4 mt-9 mb-4 ml-8">
+      <div className="flex flex-wrap gap-4 mt-9 mb-4 ml-4 sm:ml-8 justify-center sm:justify-start">
         <div className="relative">
-          <button className="text-lg py-3 px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
+          <button className="text-sm sm:text-lg py-2 sm:py-3 px-8 sm:px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
             <img
               src="https://static-cdn.jtvnw.net/c3-vg/verticals/gaming.svg"
-              className="w-10 h-10 absolute left-0 top-0 mt-2 ml-3"
+              className="w-6 h-6 sm:w-10 sm:h-10 absolute left-0 top-0 mt-2 ml-3"
               alt="icon"
             />
             GAMES
@@ -20,10 +20,10 @@ function Categories() {
         </div>
 
         <div className="relative">
-          <button className="text-lg py-3 px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
+          <button className="text-sm sm:text-lg py-2 sm:py-3 px-8 sm:px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
             <img
               src='https://static-cdn.jtvnw.net/c3-vg/verticals/irl.svg'
-              className="w-10 h-10 absolute left-0 top-0 mt-2 ml-3"
+              className="w-6 h-6 sm:w-10 sm:h-10 absolute left-0 top-0 mt-2 ml-3"
               alt="icon"
             />
             IRL
@@ -31,10 +31,10 @@ function Categories() {
         </div>
 
         <div className="relative">
-          <button className="text-lg py-3 px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
+          <button className="text-sm sm:text-lg py-2 sm:py-3 px-8 sm:px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
             <img
               src='https://static-cdn.jtvnw.net/c3-vg/verticals/music.svg'
-              className="w-10 h-10 absolute left-0 top-0 mt-2 ml-3"
+              className="w-6 h-6 sm:w-10 sm:h-10 absolute left-0 top-0 mt-2 ml-3"
               alt="icon"
             />
             Music
@@ -43,11 +43,11 @@ function Categories() {
 
         
         <div className="relative">
-          <button className="text-lg py-3 px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
+          <button className="text-sm sm:text-lg py-2 sm:py-3 px-8 sm:px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
             <img
               src='https://static-cdn.jtvnw.net/c3-vg/verticals/creative.svg'
               alt="Icon"
-              className="w-10 h-10 absolute left-0 top-0 mt-2 ml-3"
+              className="w-6 h-6 sm:w-10 sm:h-10 absolute left-0 top-0 mt-2 ml-3"
               style={{ zIndex: 5 }}
             />
             Creative
@@ -56,11 +56,11 @@ function Categories() {
 
         
         <div className="relative">
-          <button className="text-lg py-3 px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
+          <button className="text-sm sm:text-lg py-2 sm:py-3 px-8 sm:px-16 rounded-lg bg-violet-700 text-white font-sans font-medium flex items-center justify-center relative">
             <img
               src='https://static-cdn.jtvnw.net/c3-vg/verticals/esports.svg'
               alt="Icon"
-              className="w-10 h-10 absolute left-0 top-0 mt-2 ml-3"
+              className="w-6 h-6 sm:w-10 sm:h-10 absolute left-0 top-0 mt-2 ml-3"
             />
             Esports
           </button>
