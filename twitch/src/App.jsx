8ee27ace120a18ede5a82cbee578b503087c2be9
Luis Casas <luisscasas45@gmail.com>
2024-06-15 16:50:08 -0600

    FEAT/ responsive test

--- twitch/src/App.jsx
+++ twitch/src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import Categories from './components/Categories';
-import Filter from './components/Filter';
-import Content from './components/Content';
-import Aside from './components/Aside'; // Asegúrate de importar Aside desde su ubicación correcta
+import React from "react";
+import NavBar from "./components/NavBar";
+import Aside from "./components/Aside";
+import Categories from "./components/Categories";
+import Filter from "./components/Filter";
+import Content from "./components/Content";
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen p-4 grid gap-8 md:grid-cols-[25%_1fr] md:grid-rows-[auto_1fr]">
-      <NavBar className="md:col-span-2" />
-      <div className="md:hidden">
+    <main className="w-full min-h-screen p-4 grid grid-cols-1 md:grid-cols-[25%_1fr] grid-rows-[auto_1fr] gap-y-8">
+      <NavBar className="col-span-2" />
+      {/* Ocultar el Aside en dispositivos pequeños (móviles) */}
+      <Aside className="hidden md:block" />
+      <div className="">
         <Categories />
         <Filter />
+        <Content />
       </div>
-      <div className="hidden md:block">
-        <Aside />
-      </div>
-      <Content />
     </main>
   );
 }
+
+
+
 
