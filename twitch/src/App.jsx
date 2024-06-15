77b75a523d25b8fab96adfea6d646bb4bc790fe7
Luis Casas <luisscasas45@gmail.com>
2024-06-15 17:00:09 -0600

    FEAT/ full desing Tailwind CSS

--- twitch/src/App.jsx
+++ twitch/src/App.jsx
@@ -7,11 +7,11 @@ import Content from "./components/Content";
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen p-4 grid grid-cols-1 md:grid-cols-[25%_1fr] grid-rows-[auto_1fr] gap-y-8">
-      <NavBar className="col-span-2" />
+    <main className="w-full min-h-screen p-2 md:p-4 grid grid-cols-1 md:grid-cols-[25%_1fr] grid-rows-[auto_1fr] gap-y-4 md:gap-y-8">
+      <NavBar className="col-span-1 md:col-span-2" />
       {/* Ocultar el Aside en dispositivos pequeños (móviles) */}
       <Aside className="hidden md:block" />
-      <div className="">
+      <div className="col-span-1">
         <Categories />
         <Filter />
         <Content />
